7b54652e8bc9d659432735f4cdc69e4cf51ab08b
István Bozsó <bozso.istvan@csfk.mta.hu>
2020-10-30 00:57:14 +0100

    working on lazy inserting

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -9,12 +9,35 @@ pub use base::Base;
 
 use crate::service::ID;
 
+pub trait Creator: Hash {
+    type Result;
+    
+    fn create(&self) -> Self::Result;
+}
+
 pub trait Database {
     type Entry;
     
     fn get(&self, id: ID) -> Option<&Self::Entry>;
     fn insert(&mut self, entry: Self::Entry) -> ID;
     fn remove(&mut self, id: ID);
+}
+
+pub trait LazyInsert: Database {
+    type Create: Creator;
+    
+    fn calc_id(&self, creator: &Self::Create) -> ID;
+    fn contains(&self, id: ID) -> bool;
+    
+    fn lazy_insert(&mut self, creator: Self::Create) -> Self::Create::Result {
+        let id = self.calc_id(&creator);
+        
+        if self.contains(id) {
+            Ok(id)
+        } else {
+            self.insert(creator.create()?)
+        }
+    }
 }
 
 pub trait Type<T> {
