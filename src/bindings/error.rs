6d0af647bc5b24403c7653350d3c8b4555687a0f
István Bozsó <bozso.istvan@csfk.mta.hu>
2021-01-29 09:59:16 +0100

    fixed compilation error for bindigs error

--- src/bindings/error.rs
+++ src/bindings/error.rs
@@ -3,21 +3,21 @@ use crate::{
 };
 
 #[derive(thiserror::Error, Debug)]
-pub enum Error {
+pub enum Error<T: std::fmt::Debug> {
     #[error("failed to lock service structure")]
     LockFail,
     #[error("error from service: {0}")]
-    Service(#[from] service::Error),
+    Service(#[from] service::Error<T>),
 }
 
 /**
  * \TODO: properly handle different error types
  */
-impl Into<service::ID> for Error {
+impl<T> Into<service::ID> for Error<T> {
     fn into(self) -> service::ID {
         3
     }
 }
 
-impl crate::Error for Error {}
+impl<T> crate::Error for Error<T> {}
 
